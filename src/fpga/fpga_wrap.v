6724ae9f5ff9d66d06f2ce9af346084d3cdec805
MisguidedBadge <petersontrey@hotmail.com>
2024-02-19 00:12:30 +0000

    yaml fix

--- src/fpga/fpga_wrap.v
+++ src/fpga/fpga_wrap.v
@@ -4,9 +4,9 @@ module tt_um_template (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
        // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    //input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    //output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    //output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
@@ -30,8 +30,8 @@ wire [0:0] fpga_io_out;
 assign uo_out [7:1] = 7'b0;
 assign fpga_io_in = ui_in;
 
-assign uio_out = 8'b0;
-assign uio_oe = 0;
+//assign uio_out = 8'b0;
+//assign uio_oe = 0;
 
 fpga_top fpga(.prog_clk(pro_clk),
  .clk(clk), .gfpga_pad_GPIN_PAD(fpga_io_in), 
